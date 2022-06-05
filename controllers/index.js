43b312a233af562b25bfc1a6c14b9f3b1a4cc555
Rekha <98908396+rekhawb@users.noreply.github.com>
2022-06-05 17:46:27 -0400

    1.create model (#3)
    * created schema, database seed, donation routes, calevent route
    
    * git push retry
    
    Co-authored-by: rekhawb <rekha.rcr@gmail.com>

--- controllers/index.js
+++ controllers/index.js
@@ -1,17 +1,7 @@
 const router = require('express').Router();
 
+/*merge again*/
 
-
-const apiRoutes = require('./api');
-const homeRoutes = require('./home-routes');
-const eventsRoutes = require('./events-routes');
-const donationRoutes = require('./donation-routes');
-
-
-router.use('/api', apiRoutes);
-router.use('/', homeRoutes);
-router.use('/events', eventsRoutes);
-router.use('/donation', donationRoutes);
 
 
 const homeRoutes = require('./home-routes');
@@ -21,8 +11,10 @@ const donationRoutes = require('./api/donation-routes');
 const caleventRoutes = require('./api/calevents-routes');
 
 
+
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
+
 //router.use('/events', eventsRoutes);
 router.use('/donation', donationRoutes);
 router.use('/calevent',caleventRoutes);
