ba4a0939a350d7a29ba1c0e68b6976672a174fa9
bhavnao <99133032+bhavnao@users.noreply.github.com>
2022-06-05 16:44:46 -0400

    created home routes (#1)

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,17 @@
 const router = require('express').Router();
-const { Resident } = require('../models');
-;
 
 
+
+const apiRoutes = require('./api');
+const homeRoutes = require('./home-routes');
+const eventsRoutes = require('./events-routes');
+const donationRoutes = require('./donation-routes');
+
+
+router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
+router.use('/events', eventsRoutes);
+router.use('/donation', donationRoutes);
 
 
 module.exports = router;
