fd9a6a54cb1b188d0d2571c70e82f080e5ff4050
Rekha <98908396+rekhawb@users.noreply.github.com>
2022-06-07 19:20:44 -0400

    2.3 git push fixes (#9)
    * 2.2SessionLoggedInFixes (#6)
    
    Co-authored-by: rekhawb <rekha.rcr@gmail.com>
    
    * created post event handlebars (#7)
    
    * 2.2SessionLoggedInFixes (#8)
    
    Co-authored-by: rekhawb <rekha.rcr@gmail.com>
    
    Co-authored-by: rekhawb <rekha.rcr@gmail.com>
    Co-authored-by: bhavnao <99133032+bhavnao@users.noreply.github.com>

--- controllers/index.js
+++ controllers/index.js
@@ -6,19 +6,11 @@ const router = require('express').Router();
 const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes');
 
-//const eventsRoutes = require('./events-routes');
-//const donationRoutes = require('./api/donation-routes');
-//const caleventRoutes = require('./api/calevents-routes');
+
+
 
 
 router.use('/', homeRoutes);
-
-router.use('/api', apiRoutes);
-
-
-//router.use('/events', eventsRoutes);
-//router.use('/donation', donationRoutes);
-//router.use('/calevent',caleventRoutes);
 
 
 
