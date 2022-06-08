3bbd34dbf0b6a5ae53adea815640ddc10ae06e4c
Bhavna Jain <bhavnaostwal@gmail.com>
2022-06-07 21:35:44 -0400

    added post handerbars and post routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,14 +2,14 @@ const router = require('express').Router();
 const donationRoutes = require('./donation-routes');
 const caleventRoutes = require('./calevents-routes');
 const residentRoutes = require('./resident-routes');
+const postRoutes = require('./post-routes');
 
 
 
 router.use('/donation', donationRoutes);
 
 router.use('/calevent', caleventRoutes);
-
-
 router.use('/resident',residentRoutes);
+router.use('/posts', postRoutes);
 
 module.exports = router;
