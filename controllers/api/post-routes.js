67c5b068ca8f7f1a29406ae3e7b0a930c298e7e5
Bhavna Jain <bhavnaostwal@gmail.com>
2022-06-12 19:39:54 -0400

    added edit post functionality

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const { Eventpost, Resident, Usercomment } = require('../../models');
 const sequelize = require('../../config/connection');
+const withAuth = require('../../utils/auth');
 
 
 router.get('/', async (req, res) => {
@@ -18,21 +19,17 @@ router.get('/', async (req, res) => {
 
                 {
                   model: Usercomment,
-                  attributes: ['comment_id', 'description', 'post_id', 'res_id'],
+                  attributes: ['comment_id', 'description', 'post_id', 'resident_id'],
                   include: {
                     model: Resident,
                     attributes: ['name']
                   }
-                },
-             {
-                  model: Resident,
-                  attributes: ['name']
-                },
+                }
+             
               ]
         })
          //console.log(dbPostData);
         // dbPostdata = res.status(200).json(dbPostData);
-
 
         const dbData = dbPostData.map((post) =>
             post.get({ plain: true })
@@ -49,6 +46,90 @@ router.get('/', async (req, res) => {
 
 });
 
+router.get('/:id', (req, res) => {
+    Eventpost.findOne({
+      where: {
+        post_id: req.params.id
+      },
+      attributes: [
+        'post_id',
+        'description',        
+      ],
+      include: [
+        // include the Comment model here:
+        {
+          model: Resident,
+          attributes: ['name']
+        },
+        // {
+        //   model: Comment,
+        //   attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        //   include: {
+        //     model: User,
+        //     attributes: ['username']
+        //   }
+        // }
+      ]
+    })
+      .then(dbPostData => {
+        if (!dbPostData) {
+          res.status(404).json({ message: 'No post found with this id' });
+          return;
+        }
+        res.json(dbPostData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+  //==============================================================================
+
+//get the event by id
+router.get('/edit/:id', (req, res) => {
+    Eventpost.findOne({
+      where: {
+        post_id: req.params.id
+      },
+      attributes: [
+        'post_id',
+        'name',
+        'description'       
+              ],
+      include: [
+        // include the Comment model here:
+        // {
+        //   model: Resident,
+        //   attributes: ['name']
+        // },
+        // {
+        //   model: Usercomment,
+        //   attributes: ['comment_id', 'comment_text', 'post_id', 'resident_id'],
+        //   include: {
+        //     model: Resident,
+        //     attributes: ['name']
+        //   }
+        // }
+      ]
+    })
+      .then(dbPostData => {
+        if (!dbPostData) {
+          res.status(404).json({ message: 'No post found with this id' });
+          return;
+        }
+           // serialize the data
+           const post = dbPostData.get({ plain: true });
+
+           res.render('editevent', {
+               post,
+               loggedIn: true
+               });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
 // add new event page
 router.get('/newevent', async (req, res) => {
 
@@ -68,6 +149,51 @@ router.post('/',async (req, res)=>{
 
 });
 
+
+
+router.put('/:id',  (req, res) => {
+    Eventpost.update({
+        name: req.body.name,
+        description: req.body.description
+      },
+      {
+        where: {
+          post_id: req.params.id
+        }
+      })
+      .then(dbPostData => {
+        if (!dbPostData) {
+          res.status(404).json({ message: 'No post found with this id' });
+          return;
+        }
+        res.json(dbPostData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+  //======================================================================================
+
+  router.delete('/:id',  (req, res) => {
+    Eventpost.destroy({
+      where: {
+        post_id: req.params.id
+      }
+    })
+      .then(dbPostData => {
+        if (!dbPostData) {
+          res.status(404).json({ message: 'No event found with this id' });
+          return;
+        }
+        res.json(dbPostData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
 
 
 
---
+++ public/js/deleteevent.js
@@ -0,0 +1,31 @@
+async function deleteFormHandler(event) {
+    event.preventDefault();
+    
+    const id = window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1
+      ];
+
+      console.log("delete js");
+    const response = await fetch(`/api/posts/${id}`, {
+        method: 'DELETE',
+        body: JSON.stringify({
+          post_id: id
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+      
+      console.log("ok");
+
+      if (response.ok) {
+        document.location.replace('/api/posts');
+      } else {
+        alert(response.statusText);
+      }
+
+      console.log("end");
+    
+  }
+  
+  document.querySelector('.delete-post-btn').addEventListener('click', deleteFormHandler);
